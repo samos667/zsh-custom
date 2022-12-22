7ba99f203aa44f896d91218fe4e93a157d61b6d8
samos667 <50653464+samos667@users.noreply.github.com>
2022-12-22 21:54:57 +0100

    Get dependencies

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt install -y zsh
+sudo apt install -y zsh curl git
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 cp ./.zshrc ~/
