7a965f09360e9780c35acc9e3f5b066953c53b31
samos667 <50653464+samos667@users.noreply.github.com>
2022-12-23 15:50:40 +0100

    Make silent install
    https://github.com/ohmyzsh/ohmyzsh/issues/5873#issuecomment-498678076

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo apt install -y zsh curl git
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 cp ./.zshrc ~/
