982e5f6d95274a9bfffbd6f4447aa4db6b4963b3
samos667 <50653464+samos667@users.noreply.github.com>
2023-02-15 13:54:37 +0100

    set default shell to zsh

--- install_zsh.sh
+++ install_zsh.sh
@@ -4,3 +4,4 @@ sudo apt install -y zsh curl git
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 cp ./.zshrc ~/
+chsh -s $(which zsh)
